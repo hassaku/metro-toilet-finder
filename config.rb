12e2a0d0bd16b4806b51bdbf62b68b83d4b688e7
hasuo <t.hasuo@everyleaf.com>
2014-10-24 23:38:29 +0900

    initial gh-pages commit

---
+++ config.rb
@@ -0,0 +1,87 @@
+###
+# Compass
+###
+
+# Change Compass configuration
+compass_config do |config|
+  config.output_style = :expanded
+  config.line_comments = false
+end
+
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+# page "/path/to/file.html", :layout => false
+#
+# With alternative layout
+# page "/path/to/file.html", :layout => :otherlayout
+#
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
+
+# Proxy pages (http://middlemanapp.com/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
+#  :which_fake_page => "Rendering a fake page with a local variable" }
+
+###
+# Helpers
+###
+
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
+
+# Reload the browser automatically whenever files change
+set :file_watcher_ignore, [
+    /^\.idea\//,
+    /^\.bundle\//,
+    /^\.sass-cache\//,
+    /^\.git\//,
+    /^\.gitignore$/,
+    /\.DS_Store/,
+    /^build\//,
+    /^\.rbenv-.*$/,
+    /^Gemfile$/,
+    /^Gemfile\.lock$/,
+    /~$/,
+    /(^|\/)\.?#/
+]
+activate :livereload
+
+set :css_dir, 'stylesheets'
+set :js_dir, 'javascripts'
+set :images_dir, 'images'
+set :relative_links, true
+
+# Build-specific configuration
+configure :build do
+  # For example, change the Compass output style for deployment
+  # activate :minify_css
+
+  # Minify Javascript on build
+  # activate :minify_javascript
+
+  # Enable cache buster
+  # activate :asset_hash
+
+  # Use relative URLs
+  activate :relative_assets
+
+  # Or use a different image path
+  # set :http_path, "/Content/images/"
+end
+
+# Add bower's directory to sprockets asset path
+after_configuration do
+  @bower_config = JSON.parse(IO.read("#{root}/.bowerrc"))
+  sprockets.append_path File.join "#{root}", @bower_config["directory"]
+end
+
+#activate :google_analytics do |ga|
+#  ga.tracking_id = 'UA-31136981-4'
+#end
